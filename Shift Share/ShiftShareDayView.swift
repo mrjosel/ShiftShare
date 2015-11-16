1f6be81cde0207a31dfc07e7554033174e7c228f
brianjosel <brian.josel@gmail.com>
2015-11-16 16:36:07 -0500

    add ShiftShareDayView.swift

---
+++ Shift Share/ShiftShareDayView.swift
@@ -0,0 +1,35 @@
+//
+//  ShiftShareDayView.swift
+//  Shift Share
+//
+//  Created by Brian Josel on 11/16/15.
+//  Copyright © 2015 Brian Josel. All rights reserved.
+//
+
+import UIKit
+import JTCalendar
+
+class ShiftShareDayView: JTCalendarDayView {
+
+//    var dayImage: UIImageView?
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+        super.drawRect(rect)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func commonInit() {
+        super.commonInit()
+        self.backgroundColor = UIColor.greenColor()
+    }
+}
